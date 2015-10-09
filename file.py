280f485e7d966d758f6407bc70a2e7f4a372b5be
dreamer2018 <zhoupans_mail@163.com>
2015-10-09 16:30:04 +0800

    add

---
+++ file.py
@@ -0,0 +1,14 @@
+#***************************************************************************************************
+    # > File Name: file.py
+    # > Author: ZhouPan / github:dreamer2018 
+    # > Mail: zhoupans_mail@163.com
+    # > Blog: blog.csdn.net/it_dream_er
+    # > Declared: Signed on behalf of not only belongs , but also represent a responsibility!
+    # > Created Time: Fri 09 Oct 2015 04:27:51 PM CST
+#***************************************************************************************************
+
+#!/usr/bin/env python
+#coding=utf-8
+file=file('file.py','r');
+for a in file:
+    print a
