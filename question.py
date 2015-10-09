0f6a14bec025c47e8ec1b2f14538cf006cc8ae50
dreamer2018 <zhoupans_mail@163.com>
2015-10-09 18:45:16 +0800

    add

---
+++ question.py
@@ -0,0 +1,12 @@
+#***************************************************************************************************
+    # > File Name: question.py
+    # > Author: ZhouPan / github:dreamer2018 
+    # > Mail: zhoupans_mail@163.com
+    # > Blog: blog.csdn.net/it_dream_er
+    # > Declared: Signed on behalf of not only belongs , but also represent a responsibility!
+    # > Created Time: Fri 09 Oct 2015 05:13:43 PM CST
+#***************************************************************************************************
+
+#!/usr/bin/env python
+#coding=utf-8
+print 1+2*4
