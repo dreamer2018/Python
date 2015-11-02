87f44471857aca3e7d358d243b451e7a2733ac44
dreamer2018 <zhoupans_mail@163.com>
2015-11-02 09:51:46 +0800

    add

---
+++ LintCode/AmericanBigCompany/2.py
@@ -0,0 +1,15 @@
+#***************************************************************************************************
+    # > File Name: 2.py
+    # > Author: ZhouPan / github:dreamer2018 
+    # > Mail: zhoupans_mail@163.com
+    # > Blog: blog.csdn.net/it_dream_er
+    # > Declared: Signed on behalf of not only belongs , but also represent a responsibility!
+    # > Created Time: 2015年10月29日 星期四 19时56分00秒
+#***************************************************************************************************
+
+#!/usr/bin/env python
+#coding=utf-8
+def strStr(source,target):
+    for i in range(0,len(source),1):
+        if(source[i]=)
+    return -1;
