9d04953b1be83430ba21084d3ba38016777586b4
dreamer2018 <zhoupans_mail@163.com>
2015-11-30 17:36:42 +0800

    add

---
+++ readTextFile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+'readTextFile.py -- read and display text file'
+
+#get filename
+
+fname=raw_input('Enter filename:')
+
+#attempt to open file for reading
+
+try:
+    fobj=open(fname,'r')
+except IOError,e:
+    print "*** file open error:",e
+else:
+    #display contests to the screen 
+    for eachLine in fobj:
+        print eachLine,
+    fobj.close()
+
+
