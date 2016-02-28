9e6e3dc3f6755a93f0bf1a9c8d1f9c8c584938d7
dreamer2018 <zhoupans_mail@163.com>
2016-02-28 17:49:26 +0800

    v0.5

--- mysqldump.py
+++ mysqldump.py
@@ -8,8 +8,8 @@
 # Created Time: 2016年02月28日 星期日 15时35分59秒
 import os
 import time
-import threading
-import datetime
+#import threading
+
 database='zhoupan'
 username='root'
 passwd='zhoupan'
@@ -17,9 +17,32 @@ database='mysql'
 
 def mysqldump():
     dump_commond="mysqldump -u "+username+" -p"+passwd+" "+database+" > "+filename+".sql"
-    #os.system(dump_commond)
+    os.system(dump_commond)
     print dump_commond
+def gettime():
+    now_hour= int(time.strftime("%H",time.localtime(time.time())))
+    now_min = int(time.strftime("%M",time.localtime(time.time())))
+    now_sec = int(time.strftime("%S",time.localtime(time.time())))
+    return now_hour,now_min,now_sec
 filename=str(time.strftime("%Y%m%H%M%S",time.localtime(time.time())))
-#while True:
-now_hour=int(time.strftime("%H",time.localtime(time.time())))
-print now_hour
+def timeer():
+    hour,minute,second=gettime()
+    print hour
+    print minute
+    print second
+    if hour < 18:
+        sleeptime=(18-hour)*3600-minute*60-second
+        print sleeptime
+        #timer=threading.Timer(sleeptime,mysqldump)
+        #timer.start()
+        time.sleep(sleeptime)
+    else:
+        sleeptime=(24-hour)*3600-minute*60-second
+        print sleeptime
+        #timer=threading.Timer(sleeptime,mysqldump)
+        #timer.start()
+        time.sleep(sleeptime)
+def main():
+    while True:
+        timeer()
+main()
