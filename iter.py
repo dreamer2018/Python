a3d6f644d9bf12f6dc3176a97839f51bc89db0b3
dreamer2018 <zhoupans_mail@163.com>
2016-03-03 21:28:30 +0800

    add

---
+++ iter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding=utf-8
+# File Name: iter.py
+# Author: ZhouPan / github:dreamer2018 
+# Mail: zhoupans_mail@163.com
+# Blog: blog.csdn.net/it_dream_er
+# Function:
+# Created Time: 2016年03月03日 星期四 21时02分33秒
+def func():
+    print hello
+    return 5
+a=[1,2,3,4,5]
+iter(func,5)
