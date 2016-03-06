0eeded53e0542c059682e4174c7777d2f0db2335
dreamer2018 <zhoupans_mail@163.com>
2016-03-06 22:01:37 +0800

    os.path

--- os.py
+++ os.py
@@ -7,10 +7,20 @@
 # Function:
 # Created Time: 2016年03月03日 星期四 21时42分02秒
 import os
+import sys
 #os.mkdir('test')
 #区别在于rename只能更改一层，而renames可以递归式的将文件和文件夹名一次更改
 #os.renames('test/1.txt','tes/1.c')
 #os.rename('test/1.txt','tes/1.c')
-print os.getcwd()
-print os.path.basename('2q')
-print os.path.dirname('/home/zhoupan/2q')
+if(os.path.exists(sys.argv[1])):
+   print 'Exist'
+else:
+   print 'Not Exist !'
+   exit(0)
+print '文件名：',os.path.basename((sys.argv[1]))
+print '文件夹名：',os.path.dirname(os.getcwd()+sys.argv[1])
+print '最近访问时间：',os.path.getatime(sys.argv[1])
+print '文件创建时间：',os.path.getctime(sys.argv[1])
+print '文件修改时间：',os.path.getmtime(sys.argv[1])
+print '文件大小(字节):',os.path.getsize(sys.argv[1])
+
