71e01c177ee69ed498b1f07e4195731b8ceebabc
dreamer2018 <zhoupans_mail@163.com>
2016-03-07 18:41:06 +0800

    add

---
+++ mysqldump4/conf.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#coding=utf-8
+# File Name: conf.py
+# Author: ZhouPan / github:dreamer2018 
+# Mail: zhoupans_mail@163.com
+# Blog: blog.csdn.net/it_dream_er
+# Function:
+# Created Time: 2016年02月29日 星期一 16时45分38秒
+username='root'
+passwd='zhoupan'
+database='mysql'
