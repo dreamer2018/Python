a33b36beeb09474ad1d51fb63f8e0b0008535138
dreamer2018 <zhoupans_mail@163.com>
2016-03-15 16:08:20 +0800

    add

---
+++ example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#coding=utf-8
+# File Name: example.py
+# Author: ZhouPan / github:dreamer2018 
+# Mail: zhoupans_mail@163.com
+# Blog: blog.csdn.net/it_dream_er
+# Function:
+# Created Time: 2016年03月14日 星期一 22时31分21秒
+def f(x):
+    return x*x
+print(list(map(f,[2,4,6,7,8])))
