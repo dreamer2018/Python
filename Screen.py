234f504c36f4d9ab64c4f089ade826df7f368384
dreamer2018 <zhoupans_mail@163.com>
2016-05-25 12:06:31 +0800

    learn class

---
+++ Screen.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+class Screen(object):
+    @property
+    def width(self):
+        return self.width
+    @width.setter
+    def width(self,w):
+        self.width=w
+    @property
+    def height(self):
+        return self.height
+    @height.setter
+    def height(self,h):
+        self.height=h
+    @property
+    def resolution():
+        return resolution
