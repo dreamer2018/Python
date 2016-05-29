b05277d536ee9f5cf41d57cbc1b61ea0aeebefec
dreamer2018 <zhoupans_mail@163.com>
2016-05-29 12:14:39 +0800

    add

--- class.py
+++ class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
-
+'''
 class Student(object):
     def __init__(self,name,score):
         self.__name=name
@@ -9,4 +9,49 @@ class Student(object):
         print self.__name,self.__score
 s=Student('liming',58)
 s.getnamescore()
-print s._Student__name
+prnt s._Student__name
+class
+
+class test(object):
+    def __init__(self):
+        print "__init__ was called"
+    def __del__(self):
+class test(object):
+    def __init__(self):
+        print "__init__ was called "
+    def __new__(cls):
+        print "__new__ was called"
+        return super(test,cls).__new__(cls)
+
+t=test()
+class test(object):
+    def __init__(self):
+        print "__init__ was called"
+    def __del__(self):
+        print "__del__ was called"
+t=test()
+b=test()
+t1=t
+t2=t
+print id(t)
+print id(t1)
+print id(t2)
+print 'del t'
+del t
+print 'del t1'
+del t1
+print 'del t2'
+del t2
+print 'del b'
+del b
+'''
+class Hero(object):
+    def __init__(self):
+        self.__name='Zp'
+    def move():
+        print 'hreo'
+class I(Hero):
+    pass
+i=I()
+i.__name
+
