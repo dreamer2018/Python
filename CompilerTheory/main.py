6f0af47ad8d8a9fbe89bbd701737e78262b1a529
dreamer2018 <zhoupans_mail@163.com>
2016-10-11 00:41:52 +0800

    add

---
+++ CompilerTheory/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3.4
+keywards = {}
+keywards['False'] = 101
+keywards['class'] = 102
+keywards['finally'] = 103
+keywards['is'] = 104
+keywards['return'] = 105
+keywards['None'] = 106
+keywards['continue'] = 107
+keywards['for'] = 108
+keywards['lambda'] = 109
+keywards['try'] = 110
+keywards['True'] = 111
+keywards['def'] = 112
+keywards['from'] = 113
+keywards['nonlocal'] = 114
+keywards['while'] = 115
+keywards['and'] = 116
+keywards['del'] = 117
+keywards['global'] = 118
+keywards['not'] = 119
+keywards['with'] = 120
+keywards['as'] = 121
+keywards['elif'] = 122
+keywards['if'] = 123
+keywards['or'] = 124
+keywards['yield'] = 125
+keywards['assert'] = 126
+keywards['else'] = 127
+keywards['import'] = 128
+keywards['pass'] = 129
+keywards['break'] = 130
+keywards['except'] = 131
+keywards['in'] = 132
+keywards['raise'] = 133
+
+print(keywards['in'])
