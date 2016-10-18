eda55dd49d4b405cf434bdb96357bdf4c4856107
dreamer2018 <zhoupans_mail@163.com>
2016-10-18 21:22:05 +0800

    添加文件

---
+++ blog2.7.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import keyword
+import string
+
+
+def is_signal(s):
+    kw = keyword.kwlist
+    if s in kw:
+        return 0
+    elif s[0] == '_' or s[0] in string.letters:  # 判断是否为字母或下划线开头
+        for i in s:
+            if i == '_' or i in string.letters or i in string.digits:  # 判断是否由字母数字或下划线组成
+                pass
+            else:
+                return 0
+        return 1
+    else:
+        return 0
+
+
+def main():
+    s = raw_input()
+    if is_signal(s) == 1:
+        print "True"
+    else:
+        print "False"
+
+
+if __name__ == '__main__':
+    main()
